933bf0799744fba95f891fda627411fb41763dd7
zeppilabs <zeppilabs@protonmail.ch>
2021-08-06 14:14:02 +0200

    Implement basic log

--- yew-sample-app/src/main.rs
+++ yew-sample-app/src/main.rs
@@ -1,4 +1,10 @@
 use yew::prelude::*;
+extern crate web_sys;
+
+#[macro_use]
+extern crate log;
+
+use wasm_bindgen::JsValue;
 
 enum Msg {
     AddOne,
@@ -16,6 +22,7 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        web_sys::console::log_1(&JsValue::from("create"));
         Self {
             link,
             value: 0,
@@ -23,6 +30,7 @@ impl Component for Model {
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        web_sys::console::log_1(&JsValue::from("Update"));
         match msg {
             Msg::AddOne => {
                 self.value += 1;
@@ -34,6 +42,7 @@ impl Component for Model {
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        web_sys::console::log_1(&JsValue::from("Change"));
         // Should only return "true" if new properties are different to
         // previously received properties.
         // This component has no properties so we will always return "false".
@@ -41,6 +50,7 @@ impl Component for Model {
     }
 
     fn view(&self) -> Html {
+        web_sys::console::log_1(&JsValue::from("View"));
         html! {
             <div>
                 <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
@@ -51,5 +61,6 @@ impl Component for Model {
 }
 
 fn main() {
+    trace!("Start");
     yew::start_app::<Model>();
 }
